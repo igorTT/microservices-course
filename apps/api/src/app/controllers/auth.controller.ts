8e1095b5bc8ae858c1c73f8497587315c3ede5f7
igor mikailov <igormikailov@gmail.com>
2023-03-22 21:50:28 +0100

    added auth api

--- apps/api/src/app/controllers/auth.controller.ts
+++ apps/api/src/app/controllers/auth.controller.ts
@@ -1,13 +1,38 @@
-import { Controller, Body, Post } from '@nestjs/common';
+import { Controller, Body, Post, UnauthorizedException } from '@nestjs/common';
 import { AccountLogin, AccountRegister } from '@microservices-course/contracts';
+import { LoginDto } from '../dtos/login.dto';
+import { RegisterDto } from '../dtos/register.dto';
+import { RMQService } from 'nestjs-rmq';
 
 @Controller('auth')
 export class AuthController {
-  constructor() {}
+  constructor(private readonly rmqService: RMQService) {}
 
   @Post('register')
-  async register(@Body() dto: AccountRegister.Request) {}
+  async register(@Body() dto: RegisterDto) {
+    try {
+      return await this.rmqService.send<
+        AccountRegister.Request,
+        AccountRegister.Response
+      >(AccountRegister.topic, dto);
+    } catch (error) {
+      if (error instanceof Error) {
+        throw new UnauthorizedException(error.message);
+      }
+    }
+  }
 
   @Post('login')
-  async login(@Body() { email, password }: AccountLogin.Request) {}
+  async login(@Body() dto: LoginDto) {
+    try {
+      return await this.rmqService.send<
+        AccountLogin.Request,
+        AccountLogin.Response
+      >(AccountLogin.topic, dto);
+    } catch (error) {
+      if (error instanceof Error) {
+        throw new UnauthorizedException(error.message);
+      }
+    }
+  }
 }
