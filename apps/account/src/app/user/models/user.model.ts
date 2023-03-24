575c94b602f7b3393cbb947cc5e2c3b4e294a4be
igor mikailov <igormikailov@gmail.com>
2023-03-24 21:27:05 +0100

    added user course

--- apps/account/src/app/user/models/user.model.ts
+++ apps/account/src/app/user/models/user.model.ts
@@ -1,6 +1,27 @@
 import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
-import { IUser, UserRole } from '@microservices-course/interfaces';
-import { Document } from 'mongoose';
+import {
+  IUser,
+  UserRole,
+  IUserCourses,
+  PurchasedState,
+} from '@microservices-course/interfaces';
+import { Document, Types } from 'mongoose';
+
+@Schema()
+export class UserCourses extends Document implements IUserCourses {
+  @Prop({
+    required: true,
+  })
+  courseId: string;
+  @Prop({
+    required: true,
+    enum: PurchasedState,
+    type: String,
+  })
+  purchasedState: PurchasedState;
+}
+
+export const UserCoursesSchema = SchemaFactory.createForClass(UserCourses);
 
 @Schema()
 export class User extends Document implements IUser {
@@ -24,6 +45,12 @@ export class User extends Document implements IUser {
     default: UserRole.Student,
   })
   role: UserRole;
+
+  @Prop({
+    type: [UserCoursesSchema],
+    _id: false,
+  })
+  courses: Types.Array<UserCourses>;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
--- libs/interfaces/src/lib/user.interface.ts
+++ libs/interfaces/src/lib/user.interface.ts
@@ -3,10 +3,22 @@ export enum UserRole {
   Student = 'Student',
 }
 
+export enum PurchasedState {
+  Started = 'Started',
+  WaitingForPayment = 'WaitingForPayment',
+  Purchased = 'Purchased',
+  Canceled = 'Canceled',
+}
+
 export interface IUser {
   _id?: string;
   displayName?: string;
   email: string;
   passwordHash: string;
   role: UserRole;
+}
+
+export interface IUserCourses {
+  courseId: string;
+  purchasedState: PurchasedState;
 }
